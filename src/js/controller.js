491f32b8b3e693937542ebf21ff5cbc8cda63910
Alberto Roldán <a-f-r@hotmail.es>
2021-05-14 23:45:33 +0200

    Make API call

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,8 +11,23 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-//Change entrypoint in package.json from index.js to index.html
+//Make first API call
 
-// Set up project
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886#'
+    );
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+showRecipe();
 
 //Load recipe from Forkify API
