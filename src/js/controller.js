0f2fa4b271f1465858e644855912382d5a750710
Alberto Roldán <a-f-r@hotmail.es>
2021-05-16 19:17:45 +0200

    Listen for a hash change event

--- src/js/controller.js
+++ src/js/controller.js
@@ -32,9 +32,12 @@ const renderSpinner = function (parentEl) {
 const showRecipe = async function () {
   try {
     //1º Loading recipe
+
+    //Get id from the hash
+    const id = window.location.hash.slice(1);
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886#'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -162,6 +165,5 @@ const showRecipe = async function () {
 
 showRecipe();
 
-//Implement loading spinner
-
-// Add some event handlers
+//Listen for a hash change event
+window.addEventListener('hashchange', showRecipe);
