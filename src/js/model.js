5f98a4b96f90ab3dc2d42f5b2661ec4c27c5b6a3
Alberto Roldán <a-f-r@hotmail.es>
2021-05-19 14:27:35 +0200

    Finish implementing pagination

--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,5 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 // State object
@@ -8,6 +8,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -55,9 +57,13 @@ export const loadSearchResults = async function (query) {
   }
 };
 
-export const getSearchResultsPage = function (page) {
-  const start = (page - 1) * 10;
-  const end = page * 10;
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
 
   return state.search.results.slice(start, end);
 };
+
+//Render the pagination buttons
